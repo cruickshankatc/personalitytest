1e43bbaa005381f3db76b869f6061d111f219146
Andrew Cruickshank <cruickshankatc@gmail.com>
2023-05-30 23:18:16 -0400

    Overhaul of the JS part. Stores the counts for each character in an array filled with  objects

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
 let body = document.body;
 let answers = document.getElementsByTagName("input"); //Grabs the input/radio values. All are grabbed regardless of the question they fall under
-let questions = document.getElementsByTagName("div"); //Each div marks a question.
-let points = [];
 
-//For Tallying
-let allChars = [0, 0, 0, 0, 0, 0]; //For tallying purposes. Order is very important: allChars[0] = Cryofire, allChars[1] = Landmine, etc.
-let allNames = ["Cryofire", "Landmine", "Dune Runner", "Brainstorm", "Triggerhappy", "Starscream"]; //At the end it simply announces who the winner is
-let allNames2 = ["cryofire", "landmine", "dunerunner", "brainstorm", "triggerhappy", "starscream"]; //Just to match the names in points[] to tally up in the tallyingUp();
+let theCharacters = [
+	{
+		name: "cryofire",
+		count: 0,
+	},
+	{
+		name: "landmine",
+		count: 0,
+	},
+	{
+		name: "dunerunner",
+		count: 0,
+	},
+	{
+		name: "brainstorm",
+		count: 0,
+	},
+	{
+		name: "triggerhappy",
+		count: 0,
+	},
+	{
+		name: "starscream",
+		count: 0,
+	},
+]
 
-function submitForm() {	
+function submitForm() {
 	for (answer of answers) {
 		if (answer.checked) {
-			points.push(answer.className); //Grabs the character name (stored in the className) of the checked option and stores it in the points[] array			
+			for (character of theCharacters) {
+				if (answer.className == character.name) {
+					character.count++;
+				}
+			}		
 		}
 	}	
 	tallyingUp();	
 }
- 
-function tallyingUp () {
-//Results from each question
-//	var point;
-	for (x = 0; x < questions.length; x++){
-//		point = window["point" + x];
-		for (y = 0; y < allChars.length; y++) {
-			if (points[x] === allNames2[y]) {
-				allChars[y]++;
-			} 
+
+function tallyingUp() {
+	let result = [];
+	result[0] = {
+		name: null,
+		count: 0,
+	};
+	for (char1 of theCharacters) {
+		for (char2 of theCharacters) {
+			if (char1.count > char2.count) {
+				if (char1.count > result[0].count) {
+					result[0] = char1;
+				} else if (char1.count == result[0].count && char1.name != result[0].name) /*THIS WILL NEED TO BE ALTERED AS MORE QUESTIONS ARE ADDED!*/ {
+					result.push(char1);
+				}
+			}
 		}
 	}
-
- //Final Tally
-	for (y = 0; y < allChars.length; y++) {
-		if (Math.max(...allChars) === allChars[y]) {
-			console.log("You are " + allNames[y]);
-		}
+	
+	if (result.length < 2) {
+		console.log(`You are ${result[0].name}!`);
+	} else {
+		console.log(`Now for the tie breaker question!`);
 	}
-
-} 
+}
